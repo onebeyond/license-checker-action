a96d442863002ca0c49e15dd628a49184c577775
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2023-07-04 10:43:52 +0200

    feat: adding first realease of the gh action

---
+++ src/action.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core')
+const wait = require('./wait')
+const execSync = require('child_process').execSync
+
+// most @actions toolkit packages have async methods
+async function run () {
+  try {
+    const failOn = core.getInput('failOn')
+    if (!failOn) {
+      throw new Error('failOn is required')
+    }
+
+    core.info(`Checking if any of these licenses are found: ${failOn} ...`)
+    execSync(`node_modules/.bin/license-checker --failOn ${failOn}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
