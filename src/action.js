c85d363daaa882fc034f54813bdfd5dec0347c7e
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2023-07-04 11:05:08 +0200

    fix: return if there is an error

--- src/action.js
+++ src/action.js
@@ -6,11 +6,15 @@ const existSync = require('fs').existsSync
 // most @actions toolkit packages have async methods
 async function run () {
   try {
-    existSync('./package.json') || core.setFailed('package.json not found')
+    if (!existSync('./package.json')) {
+      core.setFailed('package.json not found');
+      return
+    }
 
     const failOn = core.getInput('failOn')
     if (!failOn) {
       core.setFailed('failOn is required')
+      return
     }
 
     core.info(`Checking if any of these licenses are found: ${failOn} ...`)
